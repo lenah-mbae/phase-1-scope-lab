428fc25684fff6bccf75d4a6cea8a736cc731826
lenahmbae <lenahmbae22@gmail.com>
2025-03-28 01:37:11 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newBest) {
+    bestCustomer = newBest;
+  }
+
+  const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new person';
+  
+}
+
+
